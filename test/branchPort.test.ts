67e045d3e04f2b6a05b3146f90753a1a0fcb5827
Bryan <b.powell@salesforce.com>
2019-11-18 09:45:51 -0800

    added more tests

--- test/branchPort.test.ts
+++ test/branchPort.test.ts
@@ -2,17 +2,51 @@ import { createSandbox, SinonStub } from 'sinon';
 import * as util from '../src/util';
 import BranchPort from '../src/branchPort';
 import { Context } from 'probot';
-import { BASE } from '../src/constants';
+import { BASE, TMP_LOCATION } from '../src/constants';
+import { join } from 'path';
+import { getMessage } from '../src/messages';
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
 
 describe('BranchPort', () => {
   const sandbox = createSandbox();
 
   afterEach(() => sandbox.restore());
 
+  describe('setupRepo', () => {
+    let port: BranchPort;
+    let run: SinonStub<[string], string>;
+
+    beforeEach(() => {
+      port = new BranchPort({
+        // @ts-ignore
+        repo: () => ({ owner: 'testOwner', repo: 'testRepo' })
+      });
+    });
+
+    it('Should run correct commands in order', async () => {
+      run = sandbox.stub(util, 'run');
+      const processSpy = sandbox.stub(process, 'chdir');
+      const repoPath = join(TMP_LOCATION, `testOwner-testRepo`);
+      const expectedUrl =
+        'https://sfdx-backport:testtoken@github.com/testOwner/testRepo';
+
+      await port.setupRepo('testtoken');
+
+      const calls = run.getCalls();
+      expect(calls[0].args[0]).toEqual(`git clone ${expectedUrl} ${repoPath}`);
+      expect(calls[1].args[0]).toEqual(
+        `git remote add upstream ${expectedUrl}`
+      );
+      expect(calls[2].args[0]).toEqual(`git fetch upstream ${BASE}`);
+      expect(processSpy.firstCall.args[0]).toEqual(repoPath);
+    });
+  });
+
   describe('createPortBranch', () => {
     let port: BranchPort;
     let run: SinonStub<[string], string>;
-    const portBranchName = `${BASE}-port-123`;
+    const expectedBranch = `${BASE}-port-123`;
 
     beforeEach(() => {
       run = sandbox.stub(util, 'run');
@@ -23,14 +57,18 @@ describe('BranchPort', () => {
       } as Context);
     });
 
+    it('Should return expected name for port branch', () => {
+      expect(port.createPortBranch()).toEqual(expectedBranch);
+    });
+
     it('Should run correct commands in order', () => {
       port.createPortBranch();
 
       const calls = run.getCalls();
       expect(calls[0].args[0]).toEqual(`git checkout upstream/${BASE}`);
-      expect(calls[1].args[0]).toEqual(`git checkout -b ${portBranchName}`);
+      expect(calls[1].args[0]).toEqual(`git checkout -b ${expectedBranch}`);
       expect(calls[2].args[0]).toEqual(`git cherry-pick testsha`);
-      expect(calls[3].args[0]).toEqual(`git push upstream ${portBranchName}`);
+      expect(calls[3].args[0]).toEqual(`git push upstream ${expectedBranch}`);
     });
 
     it('Should handle cherry-pick conflict error', () => {
@@ -42,12 +80,53 @@ describe('BranchPort', () => {
         fail('Should have thrown an error');
       } catch (e) {
         expect(e.name).toEqual('ConflictException');
-        expect(e.portBranchName).toEqual(portBranchName);
+        expect(e.portBranchName).toEqual(expectedBranch);
         // still push the branch that was already created
         expect(run.getCall(3).args[0]).toEqual(
-          `git push upstream ${portBranchName}`
+          `git push upstream ${expectedBranch}`
         );
       }
+    });
+  });
+
+  describe('createPortRequest', () => {
+    const context = {
+      // @ts-ignore
+      repo: () => ({ owner: 'testOwner', repo: 'testRepo' }),
+      payload: {
+        pull_request: { number: 123 }
+      },
+      github: {
+        pulls: {
+          // @ts-ignore
+          create: async options => ({
+            data: { html_url: 'https://example.com' }
+          })
+        }
+      }
+    };
+    // @ts-ignore
+    const port = new BranchPort(context);
+
+    it('Should create pull request with correct parameters', async () => {
+      const create = sandbox.spy(context.github.pulls, 'create');
+
+      await port.createPortRequest('testBranch');
+
+      expect(create.firstCall.args[0]).toStrictEqual({
+        owner: 'testOwner',
+        repo: 'testRepo',
+        title: getMessage('PortRequestTitle', ['123', BASE]),
+        body: getMessage('PortRequestBody', ['123', BASE]),
+        base: BASE,
+        head: 'testBranch'
+      });
+    });
+
+    it('Should return URL for new pull request', async () => {
+      expect(await port.createPortRequest('testBranch')).toEqual(
+        'https://example.com'
+      );
     });
   });
 });
