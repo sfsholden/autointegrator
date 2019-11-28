63529fc13d793b25612ba67b7d100c2d74c2eb3d
Bryan Powell <powell.bryan@outlook.com>
2019-11-28 11:27:52 -0500

    Tests for messages (#16)

---
+++ test/messages.test.ts
@@ -0,0 +1,24 @@
+import { getMessage } from '../src/messages';
+
+describe('Messages', () => {
+  test('should fetch message with a given key', () => {
+    expect(getMessage('LogCherryPickFailed')).toEqual(
+      'Cherry pick failed due to merge conflicts'
+    );
+  });
+
+  test('should format message with string arguments', () => {
+    expect(getMessage('PortRequestTitle', ['321', 'testFeature'])).toEqual(
+      'Port #321 to the testFeature branch'
+    );
+  });
+
+  test('should throw error for missing message key', () => {
+    try {
+      getMessage('DoesNotExist');
+      fail('should have thrown an error');
+    } catch (e) {
+      expect(e.message).toEqual('Missing message for key DoesNotExist');
+    }
+  });
+});
